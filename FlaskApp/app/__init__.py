a392869eaa60e5b34d026b762c6ee2fcf11c5071
Lucas Marques Gasparino <lucas.gasparinno@gmail.com>
2016-12-15 16:52:39 -0200

    Setup pymongo and created headers of endpoints get_records and set_records

--- FlaskApp/app/__init__.py
+++ FlaskApp/app/__init__.py
@@ -1,10 +1,28 @@
+import pymongo
+import pprint
 from flask import Flask
+from pymongo import MongoClient
 
 app = Flask(__name__)
+client = MongoClient()
+db = client.teste_db
+collection = db.catalogo
+
+
 
 @app.route('/')
-
 def index():
-    return ""
+    return "Index"
 
-app.run()
+@app.route('/get_records')
+def get_records():
+    return "Get"
+
+@app.route('/insert_records')
+def insert_records():
+    return "Set"
+
+if __name__ == "__main__":
+    app.run()
+
+
