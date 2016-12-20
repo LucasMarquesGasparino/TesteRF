226d7206bb7f2b70ed385a7d2af1c612dcc7aa32
Lucas Marques Gasparino <lucas.gasparinno@gmail.com>
2016-12-20 20:46:08 -0200

    Semi-responsive; html finished

--- FlaskApp/app/__init__.py
+++ FlaskApp/app/__init__.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import pymongo
 import pprint
 import json
@@ -9,20 +11,29 @@ from flask import request
 
 app = Flask(__name__)
 
+#------------------------------
+# Conexão com o banco de dados
 client = MongoClient()
 teste_db = client.teste_db
 catalogo = teste_db.catalogo
 
+#------------------------------------------------------
+# Renderização do template (coincide com toda a página)
 @app.route('/')
 def index():
     return render_template('Index.html');
 
+#-------------------------------------------------
+# Transmissão dos dados do Flask para o AngulasJS
 @app.route('/get_records', methods=['GET'])
 def get_records():
     try:
+        # Todas as tuplas
         collection = catalogo.find()
         listaOrganizada = []
+        # Iteração por cada uma das tuplas
         for person in collection:
+            # Agrupamento de informações num tipo legível ao JSON
             personItem = {
                 'nome':person['nome'],
                 'telefone':person['telefone'],
@@ -31,17 +42,23 @@ def get_records():
             listaOrganizada.append(personItem);
     except Exception,e:
         return str(e)
+    # Serialização em JSON
     a = jsonify(listaOrganizada)
     return a
 
+#-------------------------------------------------
+# Recepção dos dados do AngularJS pelo Flask
 @app.route('/insert_records', methods=['POST'])
 def insert_records():
     try:
+        # Desserialização em JSON
         post = request.get_json(force=True)
+        # Armazenamento dos valores recebidos
         name = post.get('nome')
         phoneNumber = post.get('telefone')
         emailAdress = post.get('email')
 
+        # Inserção dos dados na consulta Catalogo
         catalogo.insert_one({
             'nome':name,
             'telefone':phoneNumber,
